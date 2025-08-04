5016b15fdacada54e13fc0cce827c6fbc48bdc3f
priyanshu1230987 <priyanshu1230987>
2025-08-04 22:01:45 +0530

    Add: Eventsmodal component

---
+++ src/components/Eventsmodal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EventsModal = () => {
+  return (
+    <div>
+      <h2>Event Details</h2>
+      <p>This is the modal content.</p>
+    </div>
+  );
+};
+
+export default EventsModal;
